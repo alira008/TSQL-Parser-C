7c220670f82355071b02c8ff8a539a837d7f6197
Ariel Lira <ariellira.al@gmail.com>
2024-08-20 10:43:42 -0700

    updated build.zig

--- build.zig
+++ build.zig
@@ -4,18 +4,28 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const target = b.standardTargetOptions(.{});
 
-    const exe = b.addExecutable(.{ .name = "tsql-parser", .root_source_file = .{ .path = "src/main.c" }, .link_libc = true, .target = target, .optimize = optimize });
-
-    exe.addCSourceFiles(&.{ "src/lexer.c", "src/arena.c", "src/common.c" }, &.{
-        "-std=c17",
-        "-pedantic",
-        "-Wall",
-        "-Wextra",
-        "-W",
-        "-Wno-missing-field-initializers",
-        "-fno-sanitize=undefined",
+    const exe = b.addExecutable(.{
+        .name = "tsql-parser",
+        // .root_source_file = b.path("src/main.c"),
+        .link_libc = true,
+        .target = target,
+        .optimize = optimize,
     });
-    exe.addIncludePath(.{ .path = "src" });
+
+    exe.addCSourceFiles(.{
+        .root = b.path("src"),
+        .files = &.{ "main.c", "lexer.c", "arena.c", "common.c" },
+        .flags = &.{
+            "-std=c23",
+            "-pedantic",
+            "-Wall",
+            "-Wextra",
+            "-W",
+            "-Wno-missing-field-initializers",
+            "-fno-sanitize=undefined",
+        },
+    });
+    exe.addIncludePath(b.path("src"));
 
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
